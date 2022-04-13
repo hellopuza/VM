7fe3a5291e09b761f9af0cf4e3fa0828d4c262a2
MrYar0s <molokan.yaroslavik@gmail.com>
2022-04-14 00:55:06 +0300

    Add parse of cmd arguments in vm main

--- VM/src/vm_main.cpp
+++ VM/src/vm_main.cpp
@@ -10,8 +10,6 @@
     return -1;
 
 void runVM(int argc, char* argv[]);
-int parseFilesNum(int argc, char* argv[]);
-char** parseFiles(int files_num, char* argv[]);
 
 int main(int argc, char* argv[])
 {
@@ -24,38 +22,11 @@ void runVM(int argc, char* argv[])
     PkmVM* pvm = nullptr;
     PNIEnv* env = nullptr;
     PkmVMInitArgs init_args;
-    init_args.files_num = parseFilesNum(argc, argv);
-    init_args.flags_num = argc - init_args.files_num - 1;
-    init_args.files_path = parseFiles(init_args.files_num, argv);
-    init_args.flags = argv + init_args.files_num;
+    parseCmd(argc, argv, init_args);
 
     PNI_createVM(&pvm, &env, &init_args);
 
     pvm->destroyVM();
     delete pvm;
     delete env;
-}
-
-int parseFilesNum(int argc, char* argv[])
-{
-    int num = 0;
-    for(int i = 1; i < argc; i++)
-    {
-        if(argv[i][0] == '-')
-        {
-            break;
-        }
-        num++;
-    }
-    return num;
-}
-
-char** parseFiles(int files_num, char* argv[])
-{
-    char** files = new char*[files_num];
-    for(int i = 0; i < files_num; i++)
-    {
-        source[i] = argv[i + 1];
-    }
-    return files;
 }
